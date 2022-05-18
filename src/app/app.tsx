4c2345119d108bba64e5c71bab40d9537038a0f4
Sergey Bernyakovich <s.bernyakovich@kmtf.kz>
2022-05-18 16:49:21 +0500

    создана начальная структура

---
+++ src/app/app.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+	return (
+		<div className='app'>
+			<div className='_container'>
+				<h1>App</h1>
+			</div>
+		</div>
+	)
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+const mode =
+	process.env.NODE_ENV === 'production' ? 'production' : 'development'
+
+const target = process.env.NODE_ENV === 'production' ? 'browserslist' : 'web'
+
+const plugins = [
+	new CleanWebpackPlugin(),
+	new HtmlWebpackPlugin({
+		template: './src/index.html',
+	}),
+	new MiniCssExtractPlugin({
+		filename: '[name].css',
+	}),
+]
+
+if (process.env.SERVE) {
+	plugins.push(new ReactRefreshWebpackPlugin())
+}
+
+module.exports = {
+	mode,
+	plugins,
+	target,
+	entry: './src/index.tsx',
+	devtool: 'source-map',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		assetModuleFilename: 'assets/[hash][ext][query]',
+		clean: true,
+	},
+	devServer: {
+		static: '/build',
+		port: 3000,
+		open: true,
+	},
+	module: {
+		rules: [
+			{ test: /\.(html)$/, use: ['html-loader'] },
+			{
+				test: /\.(s[ac]|c)ss$/i, // /\.(le|c)ss$/i если вы используете less
+				use: [
+					MiniCssExtractPlugin.loader,
+					'css-loader',
+					'postcss-loader',
+					'sass-loader',
+				],
+			},
+			{
+				test: /\.tsx?$/,
+				exclude: /node_modules/,
+				use: ['ts-loader'],
+			},
+		],
+	},
+	resolve: {
+		extensions: ['.tsx', '.ts', '.js'],
+	},
+}
